116d8a43f73029b7b81ac4c36d4854ce7ca15097
Erich Richter <erich.richter@gmail.com>
2020-06-05 09:32:34 -0700

    add js and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,69 @@
 $(document).ready(function(){
-
+    $("#click").click(function(){ 
+    $("#w").delay("fast").fadeIn()
+    $("#h").delay("slow").fadeIn()
+    $("#a").delay("slow").fadeIn()
 });
+    
+    option.text()
+    console.log(option.splitFirstQuestion)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+function Options(option1,option2,option3,option4, option5){
+    this.option1 = option1;
+    this.option2 = option2;
+    this.option3 = option3;
+    this.option4 = option4;
+    this.option5 = option5;
+    this.counterTotal = 0;
+  }
+ 
+  
+Options.prototype.text = function(){
+    const question = "what is your favorite band?"
+    this.firstQuestionEntered = ""
+
+    this.splitFirstQuestion = question.split("")
+
+   
+
+
+}
+
+
+  var option = new Options()
+  
+  Options.prototype.selections= function(){
+    
+    this.option1 = parseInt($("#beverage").val())
+    this.option2 = parseInt($("#movie").val())
+    this.option3 = parseInt($("#animal").val())
+    this.option4 = parseInt($("#color").val())
+    this.option5 = parseInt($("#music").val())     
+  
+  this.counterTotal = this.option1 + this.option2 + this.option3 + this.option4 + this.option5;
+  
+  if(this.counterTotal <= 5){
+    $("#results").text("your language is C#")
+  }else if(this.counterTotal > 5 && this.counterTotal <=10){
+    $("#results").text("your language is JavaScript")
+  }else if(this.counterTotal > 10 && this.counterTotal <=15){
+    $("#results").text("your language is Python")
+  }else if(this.counterTotal > 15 && this.counterTotal <=20){
+    $("#results").text("your language is Ruby")
+  }else{
+    $("#results").text("please choose one of each option")
+    }
+  }
+  
