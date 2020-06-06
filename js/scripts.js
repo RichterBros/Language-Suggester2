662d4372a595dbdde9b24d9a051021f88919343e
Erich Richter <erich.richter@gmail.com>
2020-06-05 21:25:16 -0700

    js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,26 @@ $(document).ready(function(){
     $("#form-group1").hide();
     $("#test1").hide();
     $("#one").hide();
-    option.secondQustionFadeOn()
+    option.secondQuestionFadeOn()
 
-
-  });
+});
 
 $("#two").click(function() {
- 
+  
   option.secondQuestionInputted();
+  $("#form-group2").hide();
+  $("#test2").hide();
+  $("#test3").show();
+  $("#two").hide();
+
+  
+  option.thirdQuestionFadeOn();
+
+});
+
+$("#three").click(function() {
+ 
+  option.thirdQuestionInputted();
 });
 
 
@@ -26,6 +38,7 @@ $("#two").click(function() {
     console.log(option.firstQuestionInputted())
     $("#test1").append(option.text1())
     $("#test2").append(option.text2())
+    $("#test3").append(option.text3())
     
     
     console.log(option.text1())
@@ -38,20 +51,34 @@ $("#two").click(function() {
     $("#firstQuestion4").delay(1000).fadeIn(200)
 
     $("#form-group1").delay(1100).fadeIn(200)
-
+    $("#one").delay(1300).fadeIn(200)
    
   });
+  
+  Options.prototype.secondQuestionFadeOn = function(){
+    $("#secondQuestion0").delay(0).fadeIn(200)
+    $("#secondQuestion1").delay(200).fadeIn(200)
+    $("#secondQuestion2").delay(600).fadeIn(200)
+    $("#secondQuestion3").delay(800).fadeIn(200)
+    $("#secondQuestion4").delay(1000).fadeIn(200)
+  
+    $("#form-group2").delay(1100).fadeIn(200)
+    $("#two").delay(1300).fadeIn(200)
+  
+  }
+  
+Options.prototype.thirdQuestionFadeOn = function(){
+  $("#thirdQuestion0").delay(0).fadeIn(200)
+  $("#thirdQuestion1").delay(200).fadeIn(200)
+  $("#thirdQuestion2").delay(600).fadeIn(200)
+  $("#thirdQuestion3").delay(800).fadeIn(200)
+  $("#thirdQuestion4").delay(1000).fadeIn(200)
 
+  $("#form-group3").delay(1100).fadeIn(200)
+  $("#three").delay(1300).fadeIn(200)
 
-Options.prototype.secondQustionFadeOn = function(){
-  $("#secondQuestion0").delay(0).fadeIn(200)
-  $("#secondQuestion1").delay(200).fadeIn(200)
-  $("#secondQuestion2").delay(600).fadeIn(200)
-  $("#secondQuestion3").delay(800).fadeIn(200)
-  $("#secondQuestion4").delay(1000).fadeIn(200)
-
-  $("#form-group2").delay(1100).fadeIn(200)
 }
+
 
 
 
@@ -84,6 +111,15 @@ console.log(this.option2)
 }
 
 
+Options.prototype.thirdQuestionInputted = function(){
+  
+  this.option3 = parseInt($("#animal").val())
+
+console.log(this.option3)
+}
+
+
+
   var option = new Options()
   
 
@@ -113,6 +149,20 @@ console.log(this.option2)
                 return this.question2
                 
           } 
+
+
+          Options.prototype.text3 = function(){
+            const question = "what is your favorite animal?"
+            const splitFirstQuestion = question.split(" ")
+            console.log(splitFirstQuestion)
+            this.question3 ="";
+            
+            for (i=0; i < splitFirstQuestion.length; i++){
+            this.question3 += "<span id='thirdQuestion" + i + "'>" + splitFirstQuestion[i] + "</span>" + "&nbsp" ;
+                }
+                  return this.question3
+                  
+            } 
         
           
         
@@ -130,7 +180,7 @@ console.log(this.option2)
     this.option4 = parseInt($("#color").val())
     this.option5 = parseInt($("#music").val())     
   
-  this.counterTotal = this.option1 + this.option2;
+  this.counterTotal = this.option1 + this.option2 + this.option3;
   
   if(this.counterTotal <= 5){
     $("#results").text("your language is C#")
