a253d7e241b8f27bd1bd1f7e191010d87412ecb0
Erich Richter <erich.richter@gmail.com>
2020-06-05 21:53:44 -0700

    add css, html, js

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,18 @@ $("#two").click(function() {
 });
 
 $("#three").click(function() {
- 
+  $("#form-group3").hide();
+  $("#test3").hide();
+  $("#test4").show();
+  $("#three").hide();
   option.thirdQuestionInputted();
+  option.fourthQuestionFadeOn();
 });
 
+$("#four").click(function() {
+ 
+  option.fourthQuestionInputted();
+});
 
   $("#submit").click(function() {
     option.selections();
@@ -39,7 +47,7 @@ $("#three").click(function() {
     $("#test1").append(option.text1())
     $("#test2").append(option.text2())
     $("#test3").append(option.text3())
-    
+    $("#test4").append(option.text4())
     
     console.log(option.text1())
     console.log(option.splitFirstQuestion)
@@ -76,6 +84,18 @@ Options.prototype.thirdQuestionFadeOn = function(){
 
   $("#form-group3").delay(1100).fadeIn(200)
   $("#three").delay(1300).fadeIn(200)
+
+}
+
+Options.prototype.fourthQuestionFadeOn = function(){
+  $("#fourthQuestion0").delay(0).fadeIn(200)
+  $("#fourthQuestion1").delay(200).fadeIn(200)
+  $("#fourthQuestion2").delay(600).fadeIn(200)
+  $("#fourthQuestion3").delay(800).fadeIn(200)
+  $("#fourthQuestion4").delay(1000).fadeIn(200)
+
+  $("#form-group4").delay(1100).fadeIn(200)
+  $("#four").delay(1300).fadeIn(200)
 
 }
 
@@ -116,6 +136,13 @@ Options.prototype.thirdQuestionInputted = function(){
   this.option3 = parseInt($("#animal").val())
 
 console.log(this.option3)
+}
+
+Options.prototype.fourthQuestionInputted = function(){
+  
+  this.option4 = parseInt($("#color").val())
+
+console.log(this.option4)
 }
 
 
@@ -163,6 +190,20 @@ console.log(this.option3)
                   return this.question3
                   
             } 
+
+
+            Options.prototype.text4 = function(){
+              const question = "what is your favorite color?"
+              const splitFirstQuestion = question.split(" ")
+              console.log(splitFirstQuestion)
+              this.question4 ="";
+              
+              for (i=0; i < splitFirstQuestion.length; i++){
+              this.question4 += "<span id='fourthQuestion" + i + "'>" + splitFirstQuestion[i] + "</span>" + "&nbsp" ;
+                  }
+                    return this.question4
+                    
+              } 
         
           
         
@@ -180,7 +221,7 @@ console.log(this.option3)
     this.option4 = parseInt($("#color").val())
     this.option5 = parseInt($("#music").val())     
   
-  this.counterTotal = this.option1 + this.option2 + this.option3;
+  this.counterTotal = this.option1 + this.option2 + this.option3 + this.option4;
   
   if(this.counterTotal <= 5){
     $("#results").text("your language is C#")
